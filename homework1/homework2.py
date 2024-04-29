2de125d157f3811a3dc10e25ddd74b9d53b8dcd4
dankas112 <168116376+dankas112@users.noreply.github.com>
2024-04-29 20:27:06 +0700

    Update homework2.py

--- homework1/homework2.py
+++ homework1/homework2.py
@@ -1,8 +1,8 @@
 name = 'Daniil'
 print('Name: ', name)
 age = 21
-print('Age: ',age)
+print('Age: ', age)
 age = age*2
 print('New Age: ', age)
 is_student = True
-print('Is student: ',is_student)
+print('Is student: ', is_student)
